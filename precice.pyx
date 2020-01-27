858a7355d289169a64585de59027eeaebafb0c5b
ishaandesai <ishaandesai@gmail.com>
2020-01-27 16:15:40 +0100

    Initial commit

--- precice.pyx
+++ precice.pyx
@@ -243,6 +243,10 @@ cdef class Interface:
         self.thisptr.readScalarData (data_id, value_index, _value)
         return _value
 
+    # Provides current preCICE version information
+    def get_version_information(self):
+        return self.thisptr.getVersionInformation ()
+
 def action_write_initial_data ():
     return SolverInterface.actionWriteInitialData()
    
