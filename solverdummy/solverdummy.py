5e220b03895663fe3356396e962075df21d07361
Kyle Davis <kyle.davis@ipvs.uni-stuttgart.de>
2020-05-06 17:01:26 +0200

    Extends solverdummy to include data transfer

--- solverdummy/solverdummy.py
+++ solverdummy/solverdummy.py
@@ -19,8 +19,15 @@ except SystemExit:
 configuration_file_name = args.configurationFileName
 participant_name = args.participantName
 mesh_name = args.meshName
+if (participant_name == 'SolverOne'):
+  DataWrite_Name='Forces'
+  DataRead_Name='Velocities'
 
-n = 1
+if (participant_name == 'SolverTwo'):
+  DataRead_Name='Forces'
+  DataWrite_Name='Velocities'
+
+n = 3               #Number of vertices
 
 solver_process_index = 0
 solver_process_size = 1
@@ -31,24 +38,41 @@ mesh_id = interface.get_mesh_id(mesh_name)
 
 dimensions = interface.get_dimensions()
 vertices = np.zeros((n, dimensions))
+readData = np.zeros((n, dimensions))
+writeData = np.zeros((n, dimensions))
+
+for x in range(0,n):
+    for y in range(0,dimensions):
+        vertices[x,y] = x
+        readData[x,y] = x 
+        writeData[x,y] = x
 
 data_indices = interface.set_mesh_vertices(mesh_id, vertices)
+
+DataRead_ID = interface.get_data_id(DataRead_Name,mesh_id)
+DataWrite_ID = interface.get_data_id(DataWrite_Name,mesh_id)
 
 dt = interface.initialize()
     
 while interface.is_coupling_ongoing():
    
     if interface.is_action_required(precice.action_write_iteration_checkpoint()):
+        interface.write_block_vector_data(DataWrite_ID, data_indices,writeData)  
         print("DUMMY: Writing iteration checkpoint")
         interface.mark_action_fulfilled(precice.action_write_iteration_checkpoint())
     
     dt = interface.advance(dt)
     
     if interface.is_action_required(precice.action_read_iteration_checkpoint()):
+        readData = interface.read_block_vector_data(DataRead_ID, data_indices)      
         print("DUMMY: Reading iteration checkpoint")
         interface.mark_action_fulfilled(precice.action_read_iteration_checkpoint())
     else:
         print("DUMMY: Advancing in time")
+
+    writeData = readData + 1
+    print("DUMMY: writeData = ", writeData)
+
     
 interface.finalize()
 print("DUMMY: Closing python solver dummy...")
