fa63997856b6a553ae19c0c151e4240439c46e4f
ishaandesai <ishaandesai@gmail.com>
2020-05-07 10:23:25 +0200

    Reformatting variables names and code

--- solverdummy/solverdummy.py
+++ solverdummy/solverdummy.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("configurationFileName", help="Name of the xml config file.", type=str)
 parser.add_argument("participantName", help="Name of the solver.", type=str)
 parser.add_argument("meshName", help="Name of the mesh.", type=str)
+args = None
 
 try:
     args = parser.parse_args()
@@ -19,15 +20,18 @@ except SystemExit:
 configuration_file_name = args.configurationFileName
 participant_name = args.participantName
 mesh_name = args.meshName
-if (participant_name == 'SolverOne'):
-  DataWrite_Name='Forces'
-  DataRead_Name='Velocities'
 
-if (participant_name == 'SolverTwo'):
-  DataRead_Name='Forces'
-  DataWrite_Name='Velocities'
+write_data_name, read_data_name = None, None
 
-n = 3               #Number of vertices
+if participant_name == 'SolverOne':
+    write_data_name = 'Forces'
+    read_data_name = 'Velocities'
+
+if participant_name == 'SolverTwo':
+    read_data_name = 'Forces'
+    write_data_name = 'Velocities'
+
+n = 3  # Number of vertices
 
 solver_process_index = 0
 solver_process_size = 1
@@ -41,30 +45,30 @@ vertices = np.zeros((n, dimensions))
 readData = np.zeros((n, dimensions))
 writeData = np.zeros((n, dimensions))
 
-for x in range(0,n):
-    for y in range(0,dimensions):
-        vertices[x,y] = x
-        readData[x,y] = x 
-        writeData[x,y] = x
+for x in range(0, n):
+    for y in range(0, dimensions):
+        vertices[x, y] = x
+        readData[x, y] = x
+        writeData[x, y] = x
 
 vertex_ids = interface.set_mesh_vertices(mesh_id, vertices)
 
-DataRead_ID = interface.get_data_id(DataRead_Name,mesh_id)
-DataWrite_ID = interface.get_data_id(DataWrite_Name,mesh_id)
+read_data_id = interface.get_data_id(read_data_name, mesh_id)
+write_data_id = interface.get_data_id(write_data_name, mesh_id)
 
 dt = interface.initialize()
     
 while interface.is_coupling_ongoing():
    
     if interface.is_action_required(precice.action_write_iteration_checkpoint()):
-        interface.write_block_vector_data(DataWrite_ID, data_indices,writeData)  
+        interface.write_block_vector_data(write_data_id, vertex_ids, writeData)
         print("DUMMY: Writing iteration checkpoint")
         interface.mark_action_fulfilled(precice.action_write_iteration_checkpoint())
     
     dt = interface.advance(dt)
     
     if interface.is_action_required(precice.action_read_iteration_checkpoint()):
-        readData = interface.read_block_vector_data(DataRead_ID, data_indices)      
+        readData = interface.read_block_vector_data(read_data_id, vertex_ids)
         print("DUMMY: Reading iteration checkpoint")
         interface.mark_action_fulfilled(precice.action_read_iteration_checkpoint())
     else:
@@ -73,7 +77,5 @@ while interface.is_coupling_ongoing():
     writeData = readData + 1
     print("DUMMY: writeData = ", writeData)
 
-    
 interface.finalize()
 print("DUMMY: Closing python solver dummy...")
-
