5fed8bdb7b43e5e915ffead9f164d9c82b74266f
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2021-03-23 18:43:58 +0100

    Overhaul spack CI (#89)
    * Experiment with architecture proposed in https://github.com/haampie-spack/ci-example.
    * Add workflow for building dependencies from https://github.com/haampie-spack/ci-example.
    * Use spack dev-build, don't use jinja.
    * Build env on a weekly basis via cron job.
    * Do build for generic architecture x86_64. #81

--- spack/repo/packages/py-pyprecice/package.py
+++ spack/repo/packages/py-pyprecice/package.py
@@ -18,7 +18,7 @@ class PyPyprecice(PythonPackage):
     maintainers = ["ajaust", "BenjaminRodenberg"]
 
     # Always prefer final version of release candidate
-    version("develop", branch="{{ branch }}")
+    version("develop", branch="develop")
     version('2.2.0.1', sha256='032fa58193cfa69e3be37557977056e8f507d89b40c490a351d17271269b25ad')
     version('2.1.1.2', sha256='363eb3eeccf964fd5ee87012c1032353dd1518662868f2b51f04a6d8a7154045')
     version("2.1.1.1", sha256="972f574549344b6155a8dd415b6d82512e00fa154ca25ae7e36b68d4d2ed2cf4")
