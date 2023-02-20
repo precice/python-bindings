bae173832bc24eb0188d5218ca4e21917db134d1
Ishaan Desai <ishaandesai@gmail.com>
2023-02-20 09:32:29 +0100

    Use "is" in python if condition instead of ==
    Co-authored-by: Frédéric Simonis <simonisfrederic@gmail.com>

--- cyprecice/cyprecice.pyx
+++ cyprecice/cyprecice.pyx
@@ -1092,7 +1092,7 @@ cdef class Interface:
         size = _vertex_ids.size
         dimensions = self.get_dimensions()
         cdef np.ndarray[np.double_t, ndim=1] _values = np.empty(size * dimensions, dtype=np.double)
-        if relative_read_time == None:
+        if relative_read_time is None:
             self.thisptr.readBlockVectorData (data_id, size, <const int*>_vertex_ids.data, <double*>_values.data)
         else:
             self.thisptr.readBlockVectorData (data_id, size, <const int*>_vertex_ids.data, relative_read_time, <double*>_values.data)
