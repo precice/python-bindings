0b056fd97ead6ef88d1ca56c038a7f433cb0b49a
Ishaan Desai <ishaandesai@gmail.com>
2023-06-13 13:51:17 +0200

    Remove merge conflict tag

--- cyprecice/cyprecice.pyx
+++ cyprecice/cyprecice.pyx
@@ -280,16 +280,6 @@ cdef class Participant:
         """
         return self.thisptr.requiresInitialData ()
 
-<<<<<<< HEAD
-
-    def requires_writing_checkpoint (self):
-        """
-        Checks if the participant is required to write an iteration checkpoint.
-
-        If true, the participant is required to write an iteration checkpoint before
-        calling advance().
-
-=======
     def requires_writing_checkpoint (self):
         """
         Checks if the participant is required to write an iteration checkpoint.
@@ -297,7 +287,6 @@ cdef class Participant:
         If true, the participant is required to write an iteration checkpoint before
         calling advance().
         
->>>>>>> develop
         preCICE refuses to proceed if writing a checkpoint is required,
         but this method isn't called prior to advance().
 
@@ -308,10 +297,6 @@ cdef class Participant:
         """
         return self.thisptr.requiresWritingCheckpoint ()
 
-<<<<<<< HEAD
-
-=======
->>>>>>> develop
     def requires_reading_checkpoint (self):
         """
         Checks if the participant is required to read an iteration checkpoint.
@@ -331,10 +316,6 @@ cdef class Participant:
         """
         return self.thisptr.requiresReadingCheckpoint ()
 
-<<<<<<< HEAD
-
-=======
->>>>>>> develop
     # mesh access
 
     def has_mesh(self, mesh_name):
